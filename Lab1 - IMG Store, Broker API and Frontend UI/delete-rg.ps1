d64e5cce560edc8bc4fe447730aa43fdb2fe6b12
Delton Fernandes <Delton.Fernandes@british-business-bank.co.uk>
2025-01-21 15:37:18 +0000

    Session 8 - End of Lab 1

---
+++ Lab1 - IMG Store, Broker API and Frontend UI/delete-rg.ps1
@@ -0,0 +1,17 @@
+﻿$userAssignedManagedIdentity = "deltonMI"
+$resourceGroupName = "delton-rg"
+$resourceGroup = "AzureAutomationRG"
+
+# Connect to Azure with user-assigned managed identity
+$azureContext = (Connect-AzAccount -Identity).context
+$identity = Get-AzUserAssignedIdentity -ResourceGroupName $resourceGroup `
+    -Name $userAssignedManagedIdentity `
+    -DefaultProfile $azureContext
+$azureContext = (Connect-AzAccount -Identity -AccountId $identity.ClientId).context
+# set and store context
+$azureContext = Set-AzContext -SubscriptionName $azureContext.Subscription `
+    -DefaultProfile $azureContext
+$azureContext
+
+
+Remove-AzResourceGroup -Name $resourceGroupName -Force
---
+++ Lab1 - IMG Store, Broker API and Frontend UI/deploy-arm-template.ps1
@@ -0,0 +1,53 @@
+﻿Param
+(
+    [Parameter (Mandatory = $true)]
+    [string] $currentStorageAccountKey,
+    
+    [Parameter (Mandatory = $true)]
+    [string] $objectid
+)
+
+
+
+$parameters = @{
+    "objectid"= $objectid
+}
+
+$userAssignedManagedIdentityName = "deltonMI"
+$newResourceGroupName = "delton-rg"
+$location = "uksouth"
+$fileShareName = "armtemplate"
+$fileName = "azuredeploy.json"
+$currentResourceGroup = "AzureAutomationRG"
+$currentStorageAccountName = "azureautomationstore"
+
+
+
+
+# Ensures you do not inherit an AzContext in your runbook
+Disable-AzContextAutosave -Scope Process
+
+# Connect to Azure with user-assigned managed identity
+$azureContext = (Connect-AzAccount -Identity).context
+$identity = Get-AzUserAssignedIdentity -ResourceGroupName $currentResourceGroup `
+    -Name $userAssignedManagedIdentityName `
+    -DefaultProfile $azureContext
+$azureContext = (Connect-AzAccount -Identity -AccountId $identity.ClientId).context
+# set and store context
+$azureContext = Set-AzContext -SubscriptionName $azureContext.Subscription `
+    -DefaultProfile $azureContext
+$azureContext
+
+$StorageContext = New-AzStorageContext -StorageAccountName $currentStorageAccountName -StorageAccountKey $currentStorageAccountKey
+$StorageContext
+Get-AzStorageFileContent -ShareName $fileShareName -Context $StorageContext -path $fileName -Destination 'C:\' -Force
+Dir
+
+# Create the rg to deploy arm template
+
+New-AzResourceGroup -Name $newResourceGroupName -Location $location -DefaultProfile $azureContext -Force
+$templateFile = Join-Path -Path 'C:\' -ChildPath $fileName
+
+New-AzResourceGroupDeployment -TemplateFile $templateFile -ResourceGroupName $newResourceGroupName -Name "PS-deployment" -TemplateParameterObject $parameters
+
+
